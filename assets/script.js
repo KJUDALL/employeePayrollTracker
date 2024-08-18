194d1378cbf07aa13af049a6a8225f26a0f4ffd8
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-08-18 11:39:25 -0400

    updated HTML, CSS and prettier style formatting

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,60 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-
+    console.log(addEmployeesBtn);
 // Collect employee data
 const collectEmployees = function () {
-  // TODO: Get user input to create and return an array of employee objects
+    // TODO: Get user input to create and return an array of employee objects
+
+    // Prompt user to enter data
+    // let firstName = prompt("Enter First Name:")
+    // let lastName = prompt("Enter Last Name:")
+    // let salary = prompt("Enter Salary:")
+
+    // console.log(`first ${firstName}, last ${lastName}, salary ${salary}`)
+
+    //console log doesn't need a ; to close it
+    // while is a loop format like a for loop or for of loop
+    // let i = 0;
+    // while (i < myArray.length) {
+    //     console.log(myArray[i]);
+    //     i++;
+    // }
+
+    // these 2 while and do while loops work the same
+    
+    // let i = 0
+    //     do {
+    //         console.log(myArray[i])
+    //     } while (i <= myArray.length);
+    //     i++;
+
+    // make sure the first entered data isn't overwritten. create another variable for tis
+    const myEmployees = [];
+
+    let first;
+    let last;
+    let salary;
+    do {
+        let first = prompt("Enter First Name:")
+        let last = prompt("Enter Last Name:")
+        let salary = prompt("Enter Salary:")
+        const newEmployee = {
+            firstName: first, 
+            lastName: last,
+            salary: salary
+        } 
+        myEmployees.push(newEmployee);
+    } while (confirm("Add Another Employee?"));
+
+    console.log(myEmployees)
+
+    // Keep adding more employees until user chooses to stop
+
+    // create array of employee's info (objects)
+
+    // every employee will have a field for name, last name, and salary
+
+    // Default the salary to $0 if not a valid number. Use an is nan function
 };
 
 // Display the average salary
@@ -17,21 +68,20 @@ const getRandomEmployee = function (employeesArray) {
 };
 
 /*
-  ====================
-  STARTER CODE
-  Do not modify any of the code below this line:
+====================
+STARTER CODE  Do not modify any of the code below this line:
 */
 
 // Display employee data in an HTML table
 const displayEmployees = function (employeesArray) {
   // Get the employee table
-  const employeeTable = document.querySelector('#employee-table');
+const employeeTable = document.querySelector('#employee-table');
 
   // Clear the employee table
-  employeeTable.innerHTML = '';
+employeeTable.innerHTML = '';
 
   // Loop through the employee data and create a row for each employee
-  for (let i = 0; i < employeesArray.length; i++) {
+for (let i = 0; i < employeesArray.length; i++) {
     const currentEmployee = employeesArray[i];
 
     const newTableRow = document.createElement('tr');
@@ -47,36 +97,36 @@ const displayEmployees = function (employeesArray) {
     const salaryCell = document.createElement('td');
     // Format the salary as currency
     salaryCell.textContent = currentEmployee.salary.toLocaleString('en-US', {
-      style: 'currency',
-      currency: 'USD',
+    style: 'currency',
+    currency: 'USD',
     });
 
     newTableRow.append(salaryCell);
 
     employeeTable.append(newTableRow);
-  }
+}
 };
 
 const trackEmployeeData = function () {
-  const employees = collectEmployees();
+const employees = collectEmployees();
 
-  console.table(employees);
+console.table(employees);
 
-  displayAverageSalary(employees);
+displayAverageSalary(employees);
 
-  console.log('==============================');
+console.log('==============================');
 
-  getRandomEmployee(employees);
+getRandomEmployee(employees);
 
-  employees.sort(function (a, b) {
+employees.sort(function (a, b) {
     if (a.lastName < b.lastName) {
-      return -1;
+return -1;
     } else {
-      return 1;
+return 1;
     }
-  });
+});
 
-  displayEmployees(employees);
+displayEmployees(employees);
 };
 
 // Add event listener to 'Add Employees' button
